936b318b4776d7861ada5c5d3d6b318e1147a077
Ainun Najib <ainunnajib@gmail.com>
2020-08-16 12:13:43 +0800

    adding alternative answer using index()

--- lostlineup.py
+++ lostlineup.py
@@ -1,3 +1,11 @@
+## alternative answer
+#n = int(input())
+#s = list(map(int, input().split()))
+#o = "1"
+#for i in range(n-1):
+#    o += (" " + str(s.index(i)+2))
+#print(o)
+
 n = int(input())
 q = [0] * n
 q[0] = 1
