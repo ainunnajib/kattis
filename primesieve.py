aa25833770ba5742004717e7e64cd8810de26754
Ainun Najib <ainunnajib@gmail.com>
2020-08-17 12:38:43 +0800

    primesieve MLE - tried to halve the memory by ignoring even numbers

--- primesieve.py
+++ primesieve.py
@@ -1,15 +1,18 @@
-from bitarray import bitarray
 n, q = map(int, input().split())
-isprime = bitarray(n+1)
-isprime.setall(True)
-isprime[0] = False
-isprime[1] = False
+#converting N to N//2 instead for space optimization
+isprime = [True] * (n//2+1)
+isprime[0] = False # N = 1 is not prime
+isprime[1] = True # N = 2, 3 are both prime
 for i in range(2,n+1):
-    if isprime[i]:
+    if i%2 == 0 and i > 2:
+        continue
+    if isprime[i//2]:
         x = i*i
         while x <= n:
-            isprime[x] = False
+            if x%2 == 1:
+                isprime[x//2] = False
             x += i
-print(sum(isprime))
+print(sum(isprime)+1)
 for _ in range(q):
-    print(int(isprime[int(input())]))
+    x = int(input())
+    print(0 if x%2 == 0 and x > 2 else int(isprime[x//2]))
