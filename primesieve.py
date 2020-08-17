3e1c87da5d6a5ee62187a9f3df056940a28fd25c
Ainun Najib <ainunnajib@gmail.com>
2020-08-17 13:21:07 +0800

    primesieve still Memory Limit Exceeded - tried 1/3 of space

--- primesieve.py
+++ primesieve.py
@@ -1,18 +1,27 @@
 n, q = map(int, input().split())
-#converting N to N//2 instead for space optimization
-isprime = [True] * (n//2+1)
-isprime[0] = False # N = 1 is not prime
-isprime[1] = True # N = 2, 3 are both prime
+#converting N to 2*N//6 + N%3 - 1 instead for space optimization
+def mini(nn):
+    return 2*(nn//6) + nn%3 - 1
+
+isprime = [True] * (n//3+1)
+isprime[0] = False #irrelevant bit
 for i in range(2,n+1):
-    if i%2 == 0 and i > 2:
+    if (i%2 == 0 or i%3 == 0) and i > 3:
         continue
-    if isprime[i//2]:
+    if i == 2 or i == 3 or isprime[mini(i)]:
         x = i*i
         while x <= n:
-            if x%2 == 1:
-                isprime[x//2] = False
+            if x%2 != 0 and x%3 != 0:
+                isprime[mini(x)] = False
             x += i
-print(sum(isprime)+1)
+print(sum(isprime)+2)
 for _ in range(q):
     x = int(input())
-    print(0 if x%2 == 0 and x > 2 else int(isprime[x//2]))
+    if x == 1:
+        print(0)
+    elif x == 2 or x == 3:
+        print(1)
+    elif x%2 == 0 or x%3 == 0:
+        print(0)
+    else:
+        print(int(isprime[mini(x)]))
