1b91d36691bfcdeea5be8a23b571e55482ffb4e5
Ainun Najib <ainunnajib@gmail.com>
2020-08-18 09:18:47 +0800

    beekeeper AC alternative answer with count() and dictionary

--- beekeeper.py
+++ beekeeper.py
@@ -15,3 +15,16 @@ while n > 0:
 	if maxstring != "":
 		print(maxstring)
 	n = int(input())
+
+## alternative answer with count() and dictionary
+#n = int(input())
+#while n != 0:
+#    d = {}
+#    maxdouble = -1
+#    for _ in range(n):
+#        p = input()
+#        x = p.count('aa') + p.count('ii') + p.count('ee') + p.count('uu') + p.count('oo') + p.count('yy')
+#        d[x] = p
+#        maxdouble = max(maxdouble, x)
+#    print(d[maxdouble])
+#    n = int(input())
