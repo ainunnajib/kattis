6355aa31719dce7b5ae6733ad43289c5d8a24e77
Ainun Najib <ainunnajib@gmail.com>
2020-08-19 12:32:07 +0800

    empleh AC - turned out I forgot to remove the debugging print()s

--- empleh.py
+++ empleh.py
@@ -16,23 +16,19 @@ board = [['...', ':::', '...', ':::', '...', ':::', '...', ':::'],
          [':::', '...', ':::', '...', ':::', '...', ':::', '...']]
 r = 'abcdefgh'
 for c in whites:
-    print(c)
     if len(c) < 3:
         c = 'P' + c
     x = r.index(c[1])
     y = 8-int(c[2])
     s = board[y][x]
     board[y][x] = s[0]+c[0].upper()+s[2]
-    print(board[y][x])
 for c in blacks:
-    print(c)
     if len(c) < 3:
         c = 'P' + c
     x = r.index(c[1])
     y = 8-int(c[2])
     s = board[y][x]
     board[y][x] = s[0]+c[0].lower()+s[2]
-    print(board[y][x])
 print("+---+---+---+---+---+---+---+---+")
 for row in board:
     print('|' + '|'.join(row) + '|')
