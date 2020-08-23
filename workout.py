0ea11510a732413805be4908934f2f2eec08dc9f
Ainun Najib <ainunnajib@gmail.com>
2020-08-23 10:07:29 +0800

    workout AC - FINALLY! not sure what corner cases made previous ones WA

--- workout.py
+++ workout.py
@@ -12,21 +12,18 @@ for i in range(30):
 
     #checking if overlap
     if currenttime >= t[i%10]:
-        if (currenttime - t[i%10]) % cycle < u[i%10]:
+        if (currenttime - t[i%10]) % cycle <= u[i%10]:
             currenttime += u[i%10] - ((currenttime - t[i%10]) % cycle) #has to wait
 
     # now using the machine, need to check if the other guy needs to wait
-    if currenttime >= t[i%10] - cycle:
-        x = currenttime
-        y = x + usages[i%10]
-        a = ((x - t[i%10]) // cycle) * cycle + t[i%10]
-        b = a + u[i%10]
-        c = a + cycle
-        d = c + u[i%10]
-
-        if (a >= x and a < y) or (b > x and b <= y) or (a <= x and b >= y) \
-        or (c >= x and c < y) or (d > x and d <= y) or (c <= x and c >= y):
-            t[i%10] = y
+    endtime = currenttime + usages[i%10]
+    if currenttime > t[i%10]: #other started before
+        t[i%10] = currenttime - (currenttime - t[i%10]) % cycle
+        if (t[i%10] + cycle < endtime): # other has to wait
+            t[i%10] = endtime
+    else: # other starts only after now
+        if t[i%10] < endtime: # other has to wait
+            t[i%10] = endtime
 
     currenttime += usages[i%10] + recoveries[i%10]
 
