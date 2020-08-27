0bb750c313fe00719b05095fada1cfc8eb47c8f1
Ainun Najib <ainunnajib@gmail.com>
2020-08-27 22:10:30 +0800

    apaxianparent AC

---
+++ apaxianparent.py
@@ -0,0 +1,9 @@
+y, p = map(str, input().split())
+if y[-1] == 'e':
+    print(y+'x'+p)
+elif y[-1] in ['a', 'i', 'o', 'u']:
+    print(y[:-1]+'ex'+p)
+elif y[-2:] == 'ex':
+    print(y+p)
+else:
+    print(y+'ex'+p)
