a88377d00d87a6f0e250344445515302d77abc7e
Ainun Najib <ainunnajib@gmail.com>
2020-08-28 22:56:02 +0800

    wimbledon MLE - expensive storage O(n^2)

--- wimbledon.py
+++ wimbledon.py
@@ -5,10 +5,17 @@ for _ in range(n):
     p, u = map(int, input().split())
     players.append(p)
     umpires.append(u)
+
+pairs = [[0 for i in range(n)] for j in range(n)]
+for i in range(n):
+    for j in range(i+1, n):
+        m = players[i]*players[j]
+        pairs[i][j] = m
+sumpairs = [sum(pairs[i]) for i in range(n)]
+
 count = 0
 for k in range(n):
     for i in range(n):
-        for j in range(i+1, n):
-            if i != j and i!= k and j!= k:
-                count += umpires[k]*players[i]*players[j]
+        if i != k:
+            count += umpires[k] * (sumpairs[i] - pairs[i][k])
 print(count)
