955be5e80d19c5d5280679ae7056ca913c669801
Ainun Najib <ainunnajib@gmail.com>
2020-08-29 18:08:13 +0800

    catalan AC - use recursive C(n) = C(n-1)*2*(2n-1)/(n+1)

--- catalan.py
+++ catalan.py
@@ -1,16 +1,6 @@
+c = [1 for i in range(5001)]
+for i in range(2, 5001):
+    c[i] = c[i-1]*2*(2*i-1)//(i+1)
 q = int(input())
 for _ in range(q):
-    x = int(input())
-    m = 2*x
-    n = x
-    c = 1
-    while m > 0 or n > 0:
-        if m > 0:
-            c *= m
-            m -= 1
-        if n > 0:
-            if c%(n*n) == 0:
-                c //= (n*n)
-                n -= 1
-    c //= (x+1)
-    print(c)
+    print(c[int(input())])
