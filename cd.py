c54a4a5f5b12836d0e8cec3b5835098b8cf1888d
Ainun Najib <ainunnajib@gmail.com>
2020-08-30 21:44:41 +0800

    cd TLE - using set(), add() and discard()

--- cd.py
+++ cd.py
@@ -4,12 +4,10 @@ from collections import defaultdict
 inputs = iter(sys.stdin.readlines())
 n, m = map(int, next(inputs).split())
 while n + m > 0:
-    d = defaultdict(int)
-    for _ in range(n+m):
-        d[int(next(inputs))] += 1
-    count = 0
-    for v in d.values():
-        if v == 2:
-            count += 1
-    print(count)
+    d = set()
+    for _ in range(n):
+        d.add(int(next(inputs)))
+    for _ in range(m):
+        d.discard(int(next(inputs)))
+    print(n-len(d))
     n, m = map(int, next(inputs).split())
