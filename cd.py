2f6f36480427c0c7299a9d05a077c22d7af5474b
Ainun Najib <ainunnajib@gmail.com>
2020-08-30 21:53:32 +0800

    cd TLE - still TLE despite many ways, this one using fact it's sorted

--- cd.py
+++ cd.py
@@ -1,13 +1,27 @@
 import sys
-from collections import defaultdict
-
 inputs = iter(sys.stdin.readlines())
 n, m = map(int, next(inputs).split())
 while n + m > 0:
-    d = set()
+    d = []
     for _ in range(n):
-        d.add(int(next(inputs)))
+        d.append(int(next(inputs)))
+    count = 0
+    i = 0
     for _ in range(m):
-        d.discard(int(next(inputs)))
-    print(n-len(d))
+        x = int(next(inputs))
+        if i >= n:
+            continue
+        if d[i] == x:
+            count += 1
+            i += 1
+        elif d[i] < x:
+            while d[i] < x:
+                i += 1
+                if i >= n:
+                    break
+            if i < n:
+                if d[i] == x:
+                    count += 1
+                    i += 1
+    print(count)
     n, m = map(int, next(inputs).split())
