46cc240b696f85791c3e46bb15705bfa06e01468
Ainun Najib <ainunnajib@gmail.com>
2020-09-05 16:54:28 +0800

    rationalsequence AC - simulate traversing up until becoming left child

--- rationalsequence.py
+++ rationalsequence.py
@@ -1,29 +1,18 @@
+import math
 t = int(input())
 for _ in range(t):
     k, s = input().split()
     p, q = map(int, s.split('/'))
-    pos = 0
-    i = 0
-    while p > 1 or q > 1:
-        if p < q:
-            q -= p
-        else:
-            p -= q
-            pos += 2**i
-        i += 1
 
-    pos += 1
-    if pos == 2**i:
-        pos = 0
-        i += 1
-
-    p, q = 1, 1
-    while i > 0:
-        i -= 1
-        if pos < 2**i:
-            q += p
-        else:
-            p += q
-        pos %= 2**i
-
-    print(k, str(p) + '/' + str(q))
+    if q == 1:
+        print(k, '1/'+str(p+1))
+    else:
+        step = 0
+        if p > q:
+            step = math.ceil((p-q)/q)
+            p -= q * step
+        q = q-p
+        p = q+p
+        if step > 0:
+            q += p * step
+        print(k, str(p)+'/'+str(q))
