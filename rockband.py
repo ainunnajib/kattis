32b3e60e363f2b9cdb7b878bff0cd36aea5e5eb6
Ainun Najib <ainunnajib@gmail.com>
2020-09-05 20:53:19 +0800

    rockband AC - realized it's always same number of songs for each member

--- rockband.py
+++ rockband.py
@@ -1,23 +1,17 @@
+from collections import defaultdict
+
 m, s = map(int, input().split())
 a = []
 for _ in range(m):
     a.append(list(map(int, input().split())))
-d = {}
-for j in range(m):
-    d[a[j][0]] = True
 
-while len(d) < s:
-    l = []
-    for k in d:
-        for j in range(m):
-            for i in range(a[j].index(k)+1):
-                if a[j][i] not in d:
-                    l.append(a[j][i])
-    if l == []:
+d = defaultdict(int)
+for i in range(s):
+    for j in range(m):
+        d[a[j][i]] += 1
+    if len(d) == i+1:
         break
-    else:
-        for x in l:
-            d[x] = True
+
 print(len(d))
 l = []
 for k in d:
