03fe7c39f3a7087a5a4706b7fbcb8148734cf720
Ainun Najib <ainunnajib@gmail.com>
2020-09-11 22:12:33 +0800

    apples AC - just use counting instead of actually appending as reading

--- apples.py
+++ apples.py
@@ -1,19 +1,27 @@
+from collections import defaultdict
 r, c = map(int, input().split())
-columns = [[] for _ in range(c)]
-buffer = [[] for _ in range(c)]
+apples = defaultdict(int)
+empty = defaultdict(int)
+columns = [[] for i in range(c)]
+
 for _ in range(r):
     s = list(input())
     for i in range(c):
         if s[i] == 'a':
-            buffer[i].append('a')
+            apples[i] += 1
         elif s[i] == '.':
-            buffer[i].insert(0, '.')
+            empty[i] += 1
         elif s[i] == '#':
-            columns[i].extend(buffer[i])
-            buffer[i] = []
+            columns[i].extend(['.' for _ in range(empty[i])])
+            columns[i].extend(['a' for _ in range(apples[i])])
+            empty[i] = 0
+            apples[i] = 0
             columns[i].append('#')
 for i in range(c):
-    columns[i].extend(buffer[i])
+    columns[i].extend(['.' for _ in range(empty[i])])
+    columns[i].extend(['a' for _ in range(apples[i])])
+    empty[i] = 0
+    apples[i] = 0
 
 for j in range(r):
     print(''.join([columns[i][j] for i in range(c)]))
