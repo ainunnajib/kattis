91c546cfe7ecebdf7b215228582646cc92780ea1
Ainun Najib <ainunnajib@gmail.com>
2020-09-16 14:18:47 +0800

    znanstvenik AC - just join the string upfront

--- znanstvenik.py
+++ znanstvenik.py
@@ -4,14 +4,19 @@ w = []
 for _ in range(r):
     w.append(input())
 
-columns = [[w[j][i] for j in range(r)] for i in range(c)]
-printed = False
+columns = [''.join([w[j][i] for j in range(r)]) for i in range(c)]
+
 candel = 0
 for j in range(r-1):
     d = defaultdict(int)
+    maxv = 0
     for i in range(c):
-        d[''.join(columns[i][j+1:])] += 1
-    if max(d.values()) > 1:
+        s = columns[i][j+1:]
+        d[s] += 1
+        maxv = max(maxv, d[s])
+        if maxv > 1:
+            break
+    if maxv > 1:
         break
     else:
         candel += 1
