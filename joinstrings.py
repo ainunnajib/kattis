6d3863b620b88ad199f6a1f91a719ba49575dced
Ainun Najib <ainunnajib@gmail.com>
2020-10-03 23:45:44 +0800

    joinstrings AC - finally not TLE, need to solve this as linked list

--- joinstrings.py
+++ joinstrings.py
@@ -1,21 +1,26 @@
 import sys
+
 inputs = iter(sys.stdin.readlines())
 n = int(next(inputs))
 l = ['']
+d = {}
+x = {}
+
 for _ in range(n):
     l.append(next(inputs).strip())
+
 a, b = 1, 1
-d = {1:[1]}
 for _ in range(n-1):
     a, b = map(int, next(inputs).split())
-    if a in d and b in d:
-        d[a].extend(d[b])
-    elif a in d:
-        d[a].append(b)
-    elif b in d:
-        d[a] = [a]
-        d[a].extend(d[b])
-    else:
-        d[a] = [a, b]
-for i in d[a]:
-    sys.stdout.write(l[i])
+    if a not in x:
+        x[a] = a
+    if b not in x:
+        x[b] = b
+
+    d[x[a]] = b
+    x[a] = x[b]
+
+print(l[a], end = '')
+while a in d:
+    a = d[a]
+    print(l[a], end = '')
