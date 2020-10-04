4b9db4f1be867da418c0344e4e0d9ef301a95361
Ainun Najib <ainunnajib@gmail.com>
2020-10-04 11:16:10 +0800

    unionfind AC with C++ with scanf/printf, C++ cin/cout & Python still TLE

--- unionfind.py
+++ unionfind.py
@@ -1,28 +1,55 @@
-n, q = map(int, input().split())
-ufds = {i:i for i in range(n)}
+from sys import stdin
+
+class UFDS:
+    def __init__(self, n):
+        self.parents = list(range(n))
+        self.ranks = [0] * n
+        self.sizes = [1] * n
+        self.numdisjoint = n
+
+    def find(self, x):
+        xp = x
+        children = []
+        while xp != self.parents[xp]:
+            children.append(xp)
+            xp = self.parents[xp]
+        for c in children:
+            self.parents[c] = xp
+        return xp
+
+    def union(self, a, b):
+        ap = self.find(a)
+        bp = self.find(b)
+        if ap == bp:
+            return
+
+        if self.ranks[ap] < self.ranks[bp]:
+            self.parents[ap] = bp
+            self.sizes[bp] += self.sizes[ap]
+        elif self.ranks[bp] < self.ranks[ap]:
+            self.parents[bp] = ap
+            self.sizes[ap] += self.sizes[bp]
+        else:
+            self.parents[bp] = ap
+            self.ranks[ap] += 1
+            self.sizes[ap] += self.sizes[bp]
+
+        self.numdisjoint -= 1
+
+    def size(self, x):
+        return self.sizes[self.find(x)]
+
+inputs = iter(stdin.readlines())
+n, q = map(int, next(inputs).split())
+u = UFDS(n)
+
 for _ in range(q):
-    s = input().split()
+    s = next(inputs).split()
     a, b = map(int, s[1:])
 
-    update = set()
-    x = ufds[a]
-    update.add(a)
-    while x != ufds[x]:
-        update.add(x)
-        x = ufds[x]
-    for node in update:
-        ufds[node] = x
-
-    update = set()
-    y = ufds[b]
-    update.add(b)
-    while y != ufds[y]:
-        update.add(y)
-        y = ufds[y]
-    for node in update:
-        ufds[node] = y
-
     if s[0] == '?':
+        x = u.find(a)
+        y = u.find(b)
         print('yes' if x == y else 'no')
     elif s[0] == '=':
-        ufds[y] = x
+        u.union(a, b)
