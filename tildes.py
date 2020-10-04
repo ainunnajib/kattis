0b875c4e5db85a106e4fa27d41c3d95900bd5ec6
Ainun Najib <ainunnajib@gmail.com>
2020-10-04 11:57:09 +0800

    tildes AC - use stdin.readlines() & stdout.write() not input() & print()

---
+++ tildes.py
@@ -0,0 +1,52 @@
+from sys import stdin, stdout
+
+class UFDS:
+    def __init__(self, n):
+        self.parents = list(range(n))
+        self.ranks = [0] * n
+        self.sizes = [1] * n
+        self.numdisjoint = n
+
+    def find(self, x):
+        xp = x
+        children = []
+        while xp != self.parents[xp]:
+            children.append(xp)
+            xp = self.parents[xp]
+        for c in children:
+            self.parents[c] = xp
+        return xp
+
+    def union(self, a, b):
+        ap = self.find(a)
+        bp = self.find(b)
+        if ap == bp:
+            return
+
+        if self.ranks[ap] < self.ranks[bp]:
+            self.parents[ap] = bp
+            self.sizes[bp] += self.sizes[ap]
+        elif self.ranks[bp] < self.ranks[ap]:
+            self.parents[bp] = ap
+            self.sizes[ap] += self.sizes[bp]
+        else:
+            self.parents[bp] = ap
+            self.ranks[ap] += 1
+            self.sizes[ap] += self.sizes[bp]
+
+        self.numdisjoint -= 1
+
+    def size(self, x):
+        return self.sizes[self.find(x)]
+
+inputs = iter(stdin.readlines())
+n, q = map(int, next(inputs).split())
+u = UFDS(n)
+for _ in range(q):
+    s = next(inputs).split()
+    a = int(s[1]) - 1
+    if s[0] == 't':
+        b = int(s[2]) - 1
+        u.union(a, b)
+    elif s[0] == 's':
+        stdout.write(str(u.size(a)) + '\n')
