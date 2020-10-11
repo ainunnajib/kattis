d47c520bff9b58f743ebde165e217e08dd564674
Ainun Najib <ainunnajib@gmail.com>
2020-10-11 12:53:09 +0800

    wheresmyinternet AC

---
+++ wheresmyinternet.py
@@ -0,0 +1,57 @@
+class UFDS:
+    def __init__(self, n):
+        self.parents = list(range(n))
+        self.ranks = [0] * n
+        self.sizes = [1] * n
+        self.numdisjoint = n
+
+    def find(self, x):
+        xp = x
+        children = []
+        while xp != self.parents[xp]:
+            children.append(xp)
+            xp = self.parents[xp]
+        for c in children:
+            self.parents[c] = xp
+        return xp
+
+    def union(self, a, b):
+        ap = self.find(a)
+        bp = self.find(b)
+        if ap == bp:
+            return
+
+        if self.ranks[ap] < self.ranks[bp]:
+            self.parents[ap] = bp
+            self.sizes[bp] += self.sizes[ap]
+        elif self.ranks[bp] < self.ranks[ap]:
+            self.parents[bp] = ap
+            self.sizes[ap] += self.sizes[bp]
+        else:
+            self.parents[bp] = ap
+            self.ranks[ap] += 1
+            self.sizes[ap] += self.sizes[bp]
+
+        self.numdisjoint -= 1
+
+    def size(self, x):
+        return self.sizes[self.find(x)]
+
+n, m = map(int, input().split())
+
+u = UFDS(n)
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    a -= 1
+    b -= 1
+
+    u.union(a, b)
+
+if u.size(0) == n:
+    print('Connected')
+else:
+    x = u.find(0)
+    for i in range(n):
+        if u.find(i) != x:
+            print(i+1)
