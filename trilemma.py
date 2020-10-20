fd4f3748ef7cde756310d3f83f1e05dae43b3784
Ainun Najib <ainunnajib@gmail.com>
2020-10-20 09:12:18 +0800

    trilemma AC - corner case precision error checking triangle inequality

---
+++ trilemma.py
@@ -0,0 +1,36 @@
+from math import sqrt
+n = int(input())
+for case in range(1, n+1):
+    x1, y1, x2, y2, x3, y3 = map(int, input().split())
+    ss = []
+    ss.append((x1-x2)**2 + (y1-y2)**2)
+    ss.append((x1-x3)**2 + (y1-y3)**2)
+    ss.append((x3-x2)**2 + (y3-y2)**2)
+    ss.sort()
+
+    aa, bb, cc = ss
+    a, b, c = sqrt(aa), sqrt(bb), sqrt(cc)
+    ans = ''
+    if a + b <= c:
+        ans = 'not a triangle'
+    elif aa == 0:
+        ans = 'not a triangle'
+    elif (x2-x1)*(y3-y1) == (x3-x1)*(y2-y1) or (x1-x2)*(y3-y2) == (x3-x2)*(y1-y2):
+        ans = 'not a triangle'
+    elif aa + bb == cc:
+        if aa == bb:
+            ans = 'isosceles right triangle'
+        else:
+            ans = 'scalene right triangle'
+    elif aa + bb < cc:
+        if aa == bb:
+            ans = 'isosceles obtuse triangle'
+        else:
+            ans = 'scalene obtuse triangle'
+    else:
+        if aa == bb or bb == cc:
+            ans = 'isosceles acute triangle'
+        else:
+            ans = 'scalene acute triangle'
+
+    print(f'Case #{case}:', ans)
