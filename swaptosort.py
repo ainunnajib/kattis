53a1f22e64fd5f27686429ea4f8a5f3373e8bf87
Ainun Najib <ainunnajib@gmail.com>
2021-06-12 20:38:56 +0800

    swaptosort AC

---
+++ swaptosort.py
@@ -0,0 +1,54 @@
+class UFDS:
+    def __init__(self, n):
+        self.parents = list(range(n))
+        self.ranks = [0] * n
+        self.sizes = [1] * n
+        self.numdisjoint = n
+
+    def find(self, x):
+        xp = x
+        children = []
+        while xp != self.parents[xp]:
+            children.append(xp)
+            xp = self.parents[xp]
+        for c in children:
+            self.parents[c] = xp
+        return xp
+
+    def union(self, a, b):
+        ap = self.find(a)
+        bp = self.find(b)
+        if ap == bp:
+            return
+
+        if self.ranks[ap] < self.ranks[bp]:
+            self.parents[ap] = bp
+            self.sizes[bp] += self.sizes[ap]
+        elif self.ranks[bp] < self.ranks[ap]:
+            self.parents[bp] = ap
+            self.sizes[ap] += self.sizes[bp]
+        else:
+            self.parents[bp] = ap
+            self.ranks[ap] += 1
+            self.sizes[ap] += self.sizes[bp]
+
+        self.numdisjoint -= 1
+
+    def size(self, x):
+        return self.sizes[self.find(x)]
+
+n, k = map(int, input().split())
+u = UFDS(n)
+for _ in range(k):
+    a, b = map(int, input().split())
+    a -= 1
+    b -= 1
+    u.union(a, b)
+
+possible = True
+for a in range(n//2):
+    b = n - 1 - a
+    if u.find(a) != u.find(b):
+        possible = False
+        break
+print('Yes' if possible else 'No')
