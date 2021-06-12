68168d590760b6d3c58a0c5364c7241bbea369db
Ainun Najib <ainunnajib@gmail.com>
2021-06-12 20:36:29 +0800

    reversebinary AC - making it one liner

--- reversebinary.py
+++ reversebinary.py
@@ -1,12 +1,1 @@
-n = int(input())
-a = []
-while n > 0:
-    a.append(n%2)
-    n = int(n/2)
-l = int(len(a))
-x = 1
-number = 0
-for i in range(l):
-    number += int(a.pop()*x)
-    x = x*2
-print(int(number))
+print(int(bin(int(input()))[2:][::-1], 2))
