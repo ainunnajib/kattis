83c7e984fb85f71fe027e320151ef2def0f4d3fa
Ainun Najib <ainunnajib@gmail.com>
2021-06-12 17:06:46 +0800

    pizzahawaii AC - just check set of pizzas are same

--- pizzahawaii.py
+++ pizzahawaii.py
@@ -1,32 +1,22 @@
 from collections import defaultdict
 t = int(input())
 for _ in range(t):
-    tonative = defaultdict(set)
-    toforeign = defaultdict(set)
+    df = defaultdict(set)
+    dn = defaultdict(set)
     n = int(input())
     for __ in range(n):
-        p = input()
-        s = input().split()
-        x = s[1:]
-        s = input().split()
-        y = s[1:]
+        pizza = input()
+        foreigns = input().split()[1:]
+        for f in foreigns:
+            df[f].add(pizza)
+        natives = input().split()[1:]
+        for n in natives:
+            dn[n].add(pizza)
 
-        for i in x:
-            if len(tonative[i]) == 0:
-                tonative[i] = set(y)
-            else:
-                tonative[i] = tonative[i].intersection(set(y))
-
-        for i in y:
-            if len(toforeign[i]) == 0:
-                toforeign[i] = set(x)
-            else:
-                toforeign[i] = toforeign[i].intersection(set(x))
-
-    for x in sorted(tonative.keys()):
-        for y in sorted(tonative[x]):
-            if x in toforeign[y]:
-                print('(' + x + ', ' + y + ')')
+    for f in sorted(df.keys()):
+        for n in sorted(dn.keys()):
+            if df[f] == dn[n]:
+                print('(' + f + ', ' + n + ')')
 
     if _ < t-1:
         print()
