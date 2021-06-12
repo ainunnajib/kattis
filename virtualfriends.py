a982f981a614804495c9ee8455476426b312e584
Ainun Najib <ainunnajib@gmail.com>
2021-06-12 20:40:08 +0800

    virtualfriends AC

--- virtualfriends.py
+++ virtualfriends.py
@@ -1,40 +1,62 @@
-from collections import defaultdict
+class UFDS:
+    def __init__(self, n):
+        self.parents = list(range(n))
+        self.ranks = [0] * n
+        self.sizes = [1] * n
+        self.numdisjoint = n
+
+    def find(self, x):
+        xp = x
+        children = []
+        while xp != self.parents[xp]:
+            children.append(xp)
+            xp = self.parents[xp]
+        for c in children:
+            self.parents[c] = xp
+        return xp
+
+    def union(self, a, b):
+        ap = self.find(a)
+        bp = self.find(b)
+        if ap == bp:
+            return
+
+        if self.ranks[ap] < self.ranks[bp]:
+            self.parents[ap] = bp
+            self.sizes[bp] += self.sizes[ap]
+        elif self.ranks[bp] < self.ranks[ap]:
+            self.parents[bp] = ap
+            self.sizes[ap] += self.sizes[bp]
+        else:
+            self.parents[bp] = ap
+            self.ranks[ap] += 1
+            self.sizes[ap] += self.sizes[bp]
+
+        self.numdisjoint -= 1
+
+    def size(self, x):
+        return self.sizes[self.find(x)]
+
 t = int(input())
 for _ in range(t):
-    size = defaultdict(int)
-    idx = {}
-    setdict = defaultdict(set)
+    d = {}
+    idx = 0
+    l = []
     n = int(input())
     for __ in range(n):
         a, b = input().split()
-        if a in idx and b in idx:
-            if idx[a] == idx[b]:
-                s = len(setdict[idx[a]])
-            else:
-                ia = idx[a]
-                ib = idx[b]
-                seta = setdict[ia]
-                setb = setdict[ib]
-                setu = seta.union(setb)
-                s = len(setu)
-                for x in idx:
-                    if idx[x] in (ia, ib):
-                        idx[x] = ia
-                        setdict[x] = setu
-        elif a in idx:
-            setdict[idx[a]].add(b)
-            idx[b] = idx[a]
-            s = len(setdict[idx[a]])
-        elif b in idx:
-            setdict[idx[b]].add(a)
-            idx[a] = idx[b]
-            s = len(setdict[idx[b]])
-        else:
-            setu = set()
-            setu.add(a)
-            setu.add(b)
-            setdict[a] = setu
-            idx[a] = a
-            idx[b] = a
-            s = 2
-        print(s)
+
+        if a not in d:
+            d[a] = idx
+            idx += 1
+        if b not in d:
+            d[b] = idx
+            idx += 1
+
+        l.append((d[a], d[b]))
+
+    u = UFDS(idx)
+    for t in l:
+        a, b = t
+        u.union(a, b)
+        print(u.size(a))
